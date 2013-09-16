e08b48fe5279f887b77eb6012b9d2692987722ae
mengning <mengning@ustc.edu.cn>
2013-09-16 16:54:15 +0800

    Lab1 exampe code

---
+++ ase2013/students.c
@@ -0,0 +1,63 @@
+
+/********************************************************************/
+/* Copyright (C) SSE-USTC, 2013-2014                                */
+/*                                                                  */
+/*  FILE NAME             :  students.c                             */
+/*  PRINCIPAL AUTHOR      :  Mengning                               */
+/*  SUBSYSTEM NAME        :  students                               */
+/*  MODULE NAME           :  students                               */
+/*  LANGUAGE              :  C                                      */
+/*  TARGET ENVIRONMENT    :  ANY                                    */
+/*  DATE OF FIRST RELEASE :  2012/12/30                             */
+/*  DESCRIPTION           :  students info manager system           */
+/********************************************************************/
+
+/*
+ * Revision log:
+ *
+ * Created by Mengning,2013/09/09
+ * Debug and Test by Mengning,2013/09/13
+ *
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Record of every student */
+typedef struct Student
+{
+    int id;
+    int name;
+    struct Student * pNext;
+}tStudent;
+
+tStudent * pHead = NULL;
+
+int main()
+{
+    tStudent * pStudent = NULL;
+    int i = 0;
+    
+    /* insert 1000 students */
+    for (i=0;i<1000;i++)
+    {
+        pStudent        = (tStudent *)malloc(sizeof(tStudent));
+        pStudent->id    = i;
+        pStudent->name  = i;
+        pStudent->next  = pHead;
+        pHead           = pStudent;
+    }
+
+    for (i=0;i<1000;i++)
+    {
+        /* random() return long int value,0 to (2^31 - 1) */
+        int id = random() % 1000; 
+        pStudent = pHead;
+        while(pStudent->id != id)
+        {
+            pStudent = pStudent->next;
+        }
+        printf("find student %d\n",pStudent->id);
+    }   
+    return 0;  
+}
