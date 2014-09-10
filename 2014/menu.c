708166f4bc40a30cbee4617d5e3592863a2491ee
mengning <mengning@ustc.edu.cn>
2014-09-09 23:17:05 -0700

    lab1.2 - basic modularity

--- 2014/menu.c
+++ 2014/menu.c
@@ -29,6 +29,8 @@ int Help();
 #define DESC_LEN    1024
 #define CMD_NUM     10
 
+/* data struct and its operations */
+
 typedef struct DataNode
 {
     char*   cmd;
@@ -37,41 +39,25 @@ typedef struct DataNode
     struct  DataNode *next;
 } tDataNode;
 
-static tDataNode head[] = 
+tDataNode* FindCmd(tDataNode * head, char * cmd)
 {
-    {"help", "this is help cmd!", Help,&head[1]},
-    {"version", "menu program v1.0", NULL, NULL}
-};
-main()
-{
-   /* cmd line begins */
-    while(1)
+    if(head == NULL || cmd == NULL)
     {
-        char cmd[CMD_MAX_LEN];
-        printf("Input a cmd number > ");
-        scanf("%s", cmd);
-        tDataNode *p = head;
-        while(p != NULL)
-        {
-            if(!strcmp(p->cmd, cmd))
-            {
-                printf("%s - %s\n", p->cmd, p->desc);
-                if(p->handler != NULL)
-                {
-                    p->handler();
-                }
-                break;
-            }
-            p = p->next;
-        }
-        if(p == NULL) 
-        {
-            printf("This is a wrong cmd!\n ");
-        }
+        return NULL;        
     }
+    tDataNode *p = head;
+    while(p != NULL)
+    {
+        if(!strcmp(p->cmd, cmd))
+        {
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
 }
 
-int Help()
+int ShowAllCmd(tDataNode * head)
 {
     printf("Menu List:\n");
     tDataNode *p = head;
@@ -80,5 +66,42 @@ int Help()
         printf("%s - %s\n", p->cmd, p->desc);
         p = p->next;
     }
+    return 0; 
+}
+
+/* menu program */
+
+static tDataNode head[] = 
+{
+    {"help", "this is help cmd!", Help,&head[1]},
+    {"version", "menu program v1.0", NULL, NULL}
+};
+
+main()
+{
+   /* cmd line begins */
+    while(1)
+    {
+        char cmd[CMD_MAX_LEN];
+        printf("Input a cmd number > ");
+        scanf("%s", cmd);
+        tDataNode *p = FindCmd(head, cmd);
+        if( p == NULL)
+        {
+            printf("This is a wrong cmd!\n ");
+            continue;
+        }
+        printf("%s - %s\n", p->cmd, p->desc); 
+        if(p->handler != NULL) 
+        { 
+            p->handler();
+        }
+   
+    }
+}
+
+int Help()
+{
+    ShowAllCmd(head);
     return 0; 
 }
